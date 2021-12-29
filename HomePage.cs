ab8de6d23dca3790fdbf837a62c95013ef048660
Amar-Jha <92252783+Amar-Jha@users.noreply.github.com>
2021-12-29 17:33:30 +0530

    Add files via upload

---
+++ HomePage.cs
@@ -0,0 +1,12 @@
+// -------------------------------------------------------------------------------------------------
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License (MIT). See LICENSE in the repo root for license information.
+// -------------------------------------------------------------------------------------------------
+
+namespace FhirDashboard.Tests.E2E.UIPages
+{
+    internal class HomePage : BasePage
+    {
+        public const string ExpectedTitle = "Home Page - FhirDashboard";
+    }
+}
---
+++ HomePageAction.cs
@@ -0,0 +1,13 @@
+// -------------------------------------------------------------------------------------------------
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License (MIT). See LICENSE in the repo root for license information.
+// -------------------------------------------------------------------------------------------------
+
+using FhirDashboard.Tests.E2E.UIPages;
+
+namespace FhirDashboard.Tests.E2E.UIPageActions
+{
+    internal class HomePageAction
+    {
+    }
+}
