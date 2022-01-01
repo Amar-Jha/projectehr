09dca26232dd9632d9adfdffaf17087a29b827bd
Amar-Jha <92252783+Amar-Jha@users.noreply.github.com>
2022-01-01 23:07:54 +0530

    Update AboutMeModule.js

--- AboutMeModule.js
+++ AboutMeModule.js
@@ -8,25 +8,7 @@ class AboutMeModule {
         var aboutMe = this;
         aboutMe.anchor.html('Loading...');
         aboutMe.configAuth.getAboutMeInfo(function (userId, fhirServerUrl, accessToken) {
-            const markup = "
-                <h2>Information about the signed in user</h2>
-                <table>
-                    <tr>
-                        <td>UPN: ${userId}</td>
-                    </tr>
-                    <tr>
-                        <td>FHIR Server URL: ${https://frt-ehr.fhir.azurehealthcareapis.com} </td>
-                    </tr>
-                    <tr>
-                        <td>
-                            Access Token:<br>
-                            <form>
-                                <textarea id="token" style="width: 20cm; height: 5cm;">${accessToken}</textarea>
-                            </form>
-                        </td>
-                    </tr>
-                </table>"
-            ;
+            const markup = " <h2>Information about the signed in user</h2>                 <table>                     <tr>                         <td>UPN: ${userId}</td>                     </tr>                     <tr>                         <td>FHIR Server URL: ${https://frt-ehr.fhir.azurehealthcareapis.com} </td>                     </tr>                     <tr>                         <td>                             Access Token:<br>                             <form>                                 <textarea id="token" style="width: 20cm; height: 5cm;">${accessToken}</textarea>                             </form>                         </td>                     </tr>                 </table>";
 
             aboutMe.anchor.html(markup);
 
